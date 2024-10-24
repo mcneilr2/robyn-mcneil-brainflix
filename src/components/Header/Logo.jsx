cc935aabcd10159f1cca8a941a030b1ffa1e11db
mcneilr2 <robyn55555@gmail.com>
2024-10-23 20:56:04 -0700

    header html elements placed into header componenent, rendering to App.  Need to parse to individual components

---
+++ src/components/Header/Logo.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/images/Brainflix-logo.svg'
+console.log(logo)
+
+export default function Logo() {
+    return (
+        <>
+        <img src={logo} alt="Logo" />;
+        </>
+    )
+}
