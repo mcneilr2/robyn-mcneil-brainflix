77cb7a2ac5ca60bcfc513e243377b071789bcc24
mcneilr2 <robyn55555@gmail.com>
2024-10-27 17:43:40 -0700

    comment form mobile and tablet styling complete

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -9,7 +9,7 @@ export default function Button(props) {
     <>
         <div className={isUpload ? "header__upload-container" : "comments__add-container"}>
             <img className={isUpload ? "header__upload-icon" : "comments__add-icon"} src={isUpload ? uploadIcon : addCommentsIcon} alt={isUpload ? "upload icon" : "add comment icon"} />
-            <button className={isUpload ? "header__upload-button" : "comments__add-button"}>COMMENT</button>
+            <button className={isUpload ? "header__upload-button" : "comments__add-button"}>{isUpload ? "UPLOAD" : "COMMENT"}</button>
         </div>        
     </>
     )
