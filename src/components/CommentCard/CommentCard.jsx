90a9998372287c0c20693347e9e1b0880e103714
mcneilr2 <robyn55555@gmail.com>
2024-10-27 22:26:57 -0700

    completed comments tablet styling

--- src/components/CommentCard/CommentCard.jsx
+++ src/components/CommentCard/CommentCard.jsx
@@ -2,12 +2,15 @@ import './CommentCard.scss'
 
 
 export default function CommentCard(props) {
+    const clickHandler = () => {
+        props.changeActiveCard(props.id)
+      }
     
     return (
     <>
-    <article className="video-comment__card">
+    <article className="video-comment__card" onClick={clickHandler}>
         <div className="video-comment__image-container">
-            <img className="video-comment__image" src="" alt="" />
+            <img className="video-comment__image" />
         </div>
         <div className="video-comment__text-container">
             <div className="video-comment__header-container">
