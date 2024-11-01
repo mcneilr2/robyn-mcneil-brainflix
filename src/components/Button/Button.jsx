caf1bc81466c9b150502bf97ea946ee965787f8c
mcneilr2 <robyn55555@gmail.com>
2024-11-01 10:16:24 -0700

    button mobile styling fixed, typography repaired on buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,5 @@
 import uploadIcon from '../../assets/icons/upload.svg';
 import addCommentsIcon from '../../assets/icons/add_comment.svg';
-import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Button.scss'
 
@@ -14,7 +13,7 @@ export default function Button({functionality}) {
 
     return (
     <>
-    <Link to={isUpload ? "/upload" : "/"}>
+    <Link to={isUpload ? "/upload" : "/"} id={isUpload ? "header__upload-button" : "comments__button"}>
         <div className={(isUpload ? "header__upload-container" : "comments__add-container")}>
                 <img className={isUpload ? "header__upload-icon" : "comments__add-icon"} src={isUpload ? uploadIcon : addCommentsIcon} alt={isUpload ? "upload icon" : "add comment icon"} />
                 {isUpload ? "UPLOAD" : "COMMENTS"}
